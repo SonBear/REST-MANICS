85916cc0a1ad9d15e4c0c9af0bb840c54c8bbc52
Emmanuel Chable <emmanuelisai_28@hotmail.com>
2021-06-12 14:40:59 -0500

    Surrogate-keys-manga-comics-added

--- resources/data.sql
+++ resources/data.sql
@@ -6,22 +6,22 @@ INSERT INTO mangas (manga_id, nombre, autor, anio_publicacion, capitulos_disponi
 VALUES (1, "Berserk", "Kentaro Miura", 1989, 363, 1);
 
 
-insert into mangas_capitulos (capitulo_id, manga_id, nombre, fecha_publicacion,total_paginas)values(
-1, 1, "The Black Swordsman", "1990-09-26", 12);
+insert into mangas_capitulos (capitulo_id,numero_capitulo, manga_id, nombre, fecha_publicacion,total_paginas)values(
+1, 1, 1, "The Black Swordsman", "1990-09-26", 12);
 
-insert into mangas_capitulos_paginas (pagina_id, capitulo_id, image_url) values (
-1, 1, "Berserk_1_004.jpg"
-
-);
-insert into mangas_capitulos_paginas (pagina_id, capitulo_id, image_url) values (
-2, 1, "Berserk_1_005.jpg"
+insert into mangas_capitulos_paginas (pagina_id,capitulo_id, numero_pagina, image_url) values (
+1, 1,1,  "Berserk_1_004.jpg"
 
 );
-insert into mangas_capitulos_paginas (pagina_id, capitulo_id, image_url) values (
-3, 1, "Berserk_1_006.jpg"
+insert into mangas_capitulos_paginas (pagina_id,capitulo_id, numero_pagina, image_url) values (
+2, 1,2,  "Berserk_1_005.jpg"
 
 );
-insert into mangas_capitulos_paginas (pagina_id, capitulo_id, image_url) values (
-4, 1, "Berserk_1_007.jpg"
+insert into mangas_capitulos_paginas (pagina_id,capitulo_id, numero_pagina, image_url) values (
+3, 1,3,  "Berserk_1_006.jpg"
+
+);
+insert into mangas_capitulos_paginas (pagina_id,capitulo_id, numero_pagina, image_url) values (
+4, 1,4,  "Berserk_1_007.jpg"
 
 );
