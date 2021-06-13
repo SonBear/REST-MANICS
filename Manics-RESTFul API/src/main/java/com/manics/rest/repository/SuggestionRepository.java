0b27a7686300a18481a2316e38f670c5abc42a4e
fabrizziorivero <Manzana22#>
2021-06-13 14:56:16 -0500

    SuggestionRepository

---
+++ Manics-RESTFul API/src/main/java/com/manics/rest/repository/SuggestionRepository.java
@@ -0,0 +1,11 @@
+package com.manics.rest.repository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.manics.rest.model.Suggestion;
+
+@Repository
+
+public interface SuggestionRepository extends CrudRepository<Suggestion, Integer>{
+
+}
