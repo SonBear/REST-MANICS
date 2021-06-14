5d72a46801160d2ec21b03e2656011ad7467b40a
Emmanuel Chable <emmanuelisai_28@hotmail.com>
2021-06-13 20:53:56 -0500

    Page Rest added

---
+++ Manics-RESTFul API/src/main/java/com/manics/rest/rest/PageRest.java
@@ -0,0 +1,58 @@
+package com.manics.rest.rest;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import com.manics.rest.mappers.PageMapper;
+import com.manics.rest.model.core.Page;
+import com.manics.rest.rest.request.PageRequest;
+import com.manics.rest.service.PageService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PageRest {
+    @Autowired
+    private PageService pageService;
+    @Autowired
+    private PageMapper pageMapper;
+
+    @GetMapping("/paginas")
+    public ResponseEntity<List<Page>> getPages(){
+        return ResponseEntity.ok().body(pageService.getPages());
+    }
+
+    @GetMapping("/paginas/{id}")
+    public ResponseEntity<Page> getPageById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(pageService.getPageById(id));
+    }
+
+    @PostMapping("/paginas")
+    public ResponseEntity<Page> createPage(@RequestBody @Valid PageRequest request) throws URISyntaxException {
+        Page page = pageService.createPage(request.getChapterId(), pageMapper.pageRequestToPage(request));
+        return ResponseEntity.created(new URI("/paginas/" + page.getPageId())).body(page);
+    }
+
+    @PutMapping("/paginas/{id}")
+    public ResponseEntity<Page> updatePage(@PathVariable Integer id, @RequestBody @Valid PageRequest request) {
+        Page page = pageService.updatePage(id, pageMapper.pageRequestToPage(request));
+        return ResponseEntity.ok().body(page);
+    }//F no funiona
+
+    @DeleteMapping("/paginas/{id}")
+    public ResponseEntity<Page> deletePage(@PathVariable Integer id) {
+        Page page = pageService.deletePage(id);
+        return ResponseEntity.ok().body(page);
+    }
+}
